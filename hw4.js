f1de1bf17080face2ba98985c37b07ac7dae5427
Gina <e.kurmeneva@gmail.com>
2020-10-24 21:35:50 +0300

    Delete Task 10

--- hw4.js
+++ hw4.js
@@ -109,19 +109,3 @@ const countDown = num => {
 }
 
 // countDown(3);
-
-
-// Task 10
-Function.prototype.myBind = function (context) {
-    var func = this;
-    return function () {
-        return func.apply(context, arguments);
-    };
-}
-
-// function addPropToNumber(number) {
-//     return this.prop + number;
-// }
-//
-// var bound = addPropToNumber.myBind({prop: 9});
-// console.log(bound(1)); // 10
